e3e6069a309e1c2410251da4083eb46b8a050776
estherraji95 <estherraji95@gmail.com>
2024-06-13 16:16:11 +0530

    Create CoverttoInt.h

---
+++ CoverttoInt.h
@@ -0,0 +1,43 @@
+char** SplitString(const char* input_string, char delimiter) {
+    int count = 0;
+    const char* ptr = input_string;
+    while (*ptr != '\0') {
+        if (*ptr++ == delimiter) {
+            count++;
+        }
+    }
+ 
+    char** tokens = (char**)malloc(sizeof(char*) * (count + 2));
+    int i = 0;
+    ptr = input_string;
+    char* token = (char*)malloc(strlen(input_string) + 1);
+    int j = 0;
+    while (*ptr != '\0') {
+        if (*ptr == delimiter) {
+            token[j] = '\0';
+            tokens[i++] = strdup(token);
+            j = 0;
+        } else {
+            token[j++] = *ptr;
+        }
+        ptr++;
+    }
+    token[j] = '\0';
+    tokens[i++] = strdup(token);
+    tokens[i] = NULL;
+    free(token);
+    return tokens;
+}
+ 
+ 
+int ConverttoInt(const char* input_string, int* value) {
+    char* endptr;
+    *value = strtol(input_string, &endptr, 10);
+    if (endptr == input_string) {
+        return 0;
+    }
+    return 1;
+}
+ 
+
+ 
