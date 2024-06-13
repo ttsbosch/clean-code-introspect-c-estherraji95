97c402602180e9bedf0b1fbf3ad23c203e5008a3
estherraji95 <estherraji95@gmail.com>
2024-06-13 16:15:00 +0530

    Update TradeProcessor.cpp

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -2,59 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
- 
+#include<TradeRecord.h>
 
  
- 
-char** SplitString(const char* input_string, char delimiter) {
-    int count = 0;
-    const char* ptr = input_string;
-    while (*ptr != '\0') {
-        if (*ptr++ == delimiter) {
-            count++;
-        }
-    }
- 
-    char** tokens = (char**)malloc(sizeof(char*) * (count + 2));
-    int i = 0;
-    ptr = input_string;
-    char* token = (char*)malloc(strlen(input_string) + 1);
-    int j = 0;
-    while (*ptr != '\0') {
-        if (*ptr == delimiter) {
-            token[j] = '\0';
-            tokens[i++] = strdup(token);
-            j = 0;
-        } else {
-            token[j++] = *ptr;
-        }
-        ptr++;
-    }
-    token[j] = '\0';
-    tokens[i++] = strdup(token);
-    tokens[i] = NULL;
-    free(token);
-    return tokens;
-}
- 
- 
-int TryConverttoInt(const char* input_string, int* value) {
-    char* endptr;
-    *value = strtol(input_string, &endptr, 10);
-    if (endptr == input_string) {
-        return 0;
-    }
-    return 1;
-}
- 
-int TryConverttoDouble(const char* input_string, double* value) {
-    char* endptr;
-    *value = strtod(input_string, &endptr);
-    if (endptr == input_string) {
-        return 0;
-    }
-    return 1;
-}
  
 void ConvertFromCSVToXML(FILE* stream) {
     char line[1024];
