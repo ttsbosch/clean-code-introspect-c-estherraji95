7bbea1609a3a4026697cbfba693bb536b511a8a0
estherraji95 <estherraji95@gmail.com>
2024-06-27 15:18:25 +0530

    Update TradeProcessor.Tests.cpp

--- TradeProcessor.Tests.cpp
+++ TradeProcessor.Tests.cpp
@@ -1,1 +1,13 @@
+#include <gtest/gtest.h>
+#include <cstdio>
+#include <cstring>
+#include "CsvToXmlTradeConverter.h"
+
+// Test cases...
+
+TEST_F(CsvToXmlTradeConverterTest, HandleMemoryAllocationFailure) {
+    testing::internal::CaptureStderr();
+    HandleMemoryAllocationFailure();
+    std::string output = testing::internal::GetCapturedStderr();
+    EXPECT_EQ(output, "Memory allocation failed.\n
 
