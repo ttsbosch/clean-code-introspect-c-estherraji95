e80986381dc7792c88b1aa4689849b591570dd40
estherraji95 <estherraji95@gmail.com>
2024-06-27 14:49:35 +0530

    Update CsvToXmlTradeConverter.cpp

--- CsvToXmlTradeConverter.cpp
+++ CsvToXmlTradeConverter.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include "TradeRecord.h"
+#include "StringProcessor.h"
+#include "CsvToXmlTradeConverter.h"
 
 int main() {
     FILE* csvFile = fopen("../trades.txt", "r");
