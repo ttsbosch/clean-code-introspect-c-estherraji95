d0ab871fa9e8c5de45702b5520051d22484b33f9
estherraji95 <estherraji95@gmail.com>
2024-06-28 13:43:47 +0530

    Update TradeProcessor.Tests.cpp

--- TradeProcessor.Tests.cpp
+++ TradeProcessor.Tests.cpp
@@ -1,1 +1,21 @@
 
+#include "gtest/gtest.h"
+#include "Converters.h"
+TEST(ConvertStringToIntTest, ValidInput) {
+    int resultValue;
+    const char* validInput = "123";
+    ASSERT_EQ(tryToConvertStringtoInt(validInput, &resultValue), 1);
+    ASSERT_EQ(resultValue, 123);
+}
+ 
+TEST(ConvertStringToIntTest, InvalidInput) {
+    int resultValue;
+    const char* invalidInput = "abc";
+    ASSERT_EQ(tryToConvertStringtoInt(invalidInput, &resultValue), 0);
+    // resultValue should remain unchanged if conversion fails
+}
+ 
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
